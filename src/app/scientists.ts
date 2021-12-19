206661c2defc26bc1a22a5c7523623ca5ed3fb25
Vivekanand P V <vivekanandpv@gmail.com>
2021-12-19 13:20:37 +0530

    exercise

---
+++ src/app/scientists.ts
@@ -0,0 +1,49 @@
+export const scientists = [
+  {
+    name: 'C V Raman',
+    country: 'India',
+    field: 'Physics',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Sir_CV_Raman.JPG/440px-Sir_CV_Raman.JPG',
+    nobelPrize: true,
+  },
+  {
+    name: 'Carl Friedrich Gauss',
+    country: 'Germany',
+    field: 'Mathematics',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Carl_Friedrich_Gauss_1840_by_Jensen.jpg/440px-Carl_Friedrich_Gauss_1840_by_Jensen.jpg',
+    nobelPrize: false,
+  },
+  {
+    name: 'Werner Heisenberg',
+    country: 'Germany',
+    field: 'Physics',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Bundesarchiv_Bild183-R57262%2C_Werner_Heisenberg.jpg/440px-Bundesarchiv_Bild183-R57262%2C_Werner_Heisenberg.jpg',
+    nobelPrize: true,
+  },
+  {
+    name: 'Lev Landau',
+    country: 'USSR',
+    field: 'Physics',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Landau.jpg/440px-Landau.jpg',
+    nobelPrize: true,
+  },
+  {
+    name: 'Alan Turing',
+    country: 'UK',
+    field: 'Computer Science',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Alan_Turing_Aged_16.jpg/440px-Alan_Turing_Aged_16.jpg',
+    nobelPrize: false,
+  },
+];
+
+//  https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/Nobel_Prize.png/440px-Nobel_Prize.png
+
+//  Exercise:
+//  Display all scientists with their own cards
+//  highlight their field
+//  decorate specially if the scientist is a nobel prize winner

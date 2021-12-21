09df74a74b2ce049745a9a510fedf7af33319964
Vivekanand P V <vivekanandpv@gmail.com>
2021-12-21 16:55:39 +0530

    response interception

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -1,4 +1,5 @@
 import {
+  HttpErrorResponse,
   HttpEvent,
   HttpHandler,
   HttpInterceptor,
@@ -19,12 +20,43 @@ export class MessageService implements HttpInterceptor {
       return next.handle(req);
     }
 
-    return next.handle(
-      req.clone({
-        setHeaders: {
-          'X-Custom-Token': 'ABCD12345',
-        },
-      })
-    );
+    return next
+      .handle(
+        req.clone({
+          setHeaders: {
+            'X-Custom-Token': 'ABCD12345',
+          },
+        })
+      )
+      .pipe(
+        //  This action of map may not be recommended
+        //  Rather questionable approach
+        //  But possible nonetheless
+        map((r) => {
+          if (r instanceof HttpResponse) {
+            console.log('Response', r);
+            const res = r as HttpResponse<any>;
+
+            return res.clone({
+              body: res.body.data, //  this is the problem (assuming body has data)
+            }) as HttpEvent<any>;
+          }
+
+          return r;
+        }),
+        //  Error handler
+        //  catchError must return the proper Observable in any case
+        catchError((err: any) => {
+          if (err instanceof HttpErrorResponse) {
+            if (err.status >= 400 && err.status < 500) {
+              alert('Server Not OK!');
+            }
+          }
+
+          //  We need to return the new empty response
+          //  even when we don't have one (because of error in communication)
+          return new Observable<HttpEvent<any>>();
+        })
+      );
   }
 }
